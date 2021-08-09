201d9aeab2495ef8de95b4b3b380d77efaaa4090
YhQIAO <970445893@qq.com>
2021-08-10 00:08:14 +0800

    add 手写高斯牛顿 BA

---
+++ ch7/my_gaussnewton_3d2d.cpp
@@ -0,0 +1,181 @@
+//
+// Created by qyh on 2021/8/9.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <Eigen/Core>
+#include <sophus/se3.hpp>
+#include <unistd.h>
+#include <chrono>
+
+using namespace std;
+using namespace cv;
+
+typedef vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d>> VecVector2d;
+typedef vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>> VecVector3d;
+
+void find_feature_matches(
+        const Mat &img_1, const Mat &img_2,
+        vector<KeyPoint> &keyPoints_1,
+        vector<KeyPoint> &keyPoints_2,
+        vector<DMatch> &matches
+        ) {
+    Mat descriptors_1, descriptors_2;
+    Ptr<FeatureDetector> detector = ORB::create();
+    Ptr<DescriptorExtractor> descriptor = ORB::create();
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(
+            DescriptorMatcher::BRUTEFORCE_HAMMING
+            );
+    detector->detect(img_1, keyPoints_1);
+    detector->detect(img_2, keyPoints_2);
+
+    descriptor->compute(img_1, keyPoints_1, descriptors_1);
+    descriptor->compute(img_2, keyPoints_2, descriptors_2);
+
+    vector<DMatch> match;
+    matcher->match(descriptors_1, descriptors_2, match);
+
+    double min_dist = 10000, max_dist = 0;
+    for(int i = 0;i <  descriptors_1.rows;i++) {
+        double dist = match[i].distance;
+        if(dist < min_dist) min_dist = dist;;
+        if(dist > max_dist) max_dist = dist;
+    }
+
+    cout << "Max dist: " << max_dist << " Min dist: " << min_dist << endl;
+
+    for(int i = 0;i < descriptors_1.rows;i++) {
+        if(match[i].distance <= max(2*min_dist, 30.0)) {
+            matches.push_back(match[i]);
+        }
+    }
+}
+
+Point2d pixel2cam(const Point2d &p, const Mat &K) {
+    return Point2d
+    (
+            (p.x - K.at<double>(0, 2)) / K.at<double>(0, 0),
+            (p.y - K.at<double>(1, 2)) / K.at<double>(1, 1)
+            );
+}
+
+void bundleAdjustmentGaussNewton(
+        const VecVector3d &points3d,
+        const VecVector2d &points2d,
+        cv::Mat &K,
+        Sophus::SE3d &pose
+        ) {
+    typedef Eigen::Matrix<double, 6,1> Vector6d;
+    const int iterations = 10;
+    double cost = 0, lastCost = 0;
+    double fx = K.at<double>(0,0);
+    double fy = K.at<double>(1, 1);
+    double cx = K.at<double>(0, 2);
+    double cy = K.at<double>(1, 2);
+
+    for(int iter = 0;iter < iterations;iter++) {
+        Eigen::Matrix<double, 6, 6> H = Eigen::Matrix<double, 6, 6>::Zero();
+        Vector6d b = Vector6d::Zero();
+
+        cost = 0;
+        for(int i = 0;i < points3d.size();i++) {
+            Eigen::Vector3d pc = pose*points3d[i];
+            double inv_z = 1.0/pc[2];
+            double inv_z2 = inv_z*inv_z;
+            Eigen::Vector2d proj(fx*pc[0] / pc[2]+cx,
+                                 fy*pc[1] / pc[2]+cy);
+            Eigen::Vector2d e = points2d[i] - proj;
+            cost += e.squaredNorm();
+            Eigen::Matrix<double, 2, 6> J;
+            J << -fx * inv_z,
+                0,
+                fx * pc[0] * inv_z2,
+                fx * pc[0] * pc[1] * inv_z2,
+                -fx - fx * pc[0] * pc[0] * inv_z2,
+                fx * pc[1] * inv_z,
+                0,
+                -fy * inv_z,
+                fy * pc[1] * inv_z2,
+                fy + fy * pc[1] * pc[1] * inv_z2,
+                -fy * pc[0] * pc[1] * inv_z2,
+                -fy * pc[0] * inv_z;
+                H+=J.transpose()*J;
+                b+= -J.transpose()*e;
+        }
+
+        Vector6d dx;
+        dx = H.ldlt().solve(b);
+
+        if (isnan(dx[0])) {
+            cout << "result is nan!" << endl;
+            break;
+        }
+
+        if (iter > 0 && cost >= lastCost) {
+            // cost increase, update is not good
+            cout << "cost: " << cost << ", last cost: " << lastCost << endl;
+            break;
+        }
+
+        pose = Sophus::SE3d::exp(dx)*pose;
+        lastCost = cost;
+
+        cout << "iteration " << iter << " cost=" << std::setprecision(12) << cost << endl;
+        if (dx.norm() < 1e-6) {
+            cout << "converge" << endl;
+            break;
+        }
+        cout << "pose by g-n: \n" << pose.matrix() << endl;
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc != 5) {
+        cout << "usage: pose_estimation_3d2d img1 img2 depth1 depth2" << endl;
+        return 1;
+    }
+    //-- 读取图像
+    Mat img_1 = imread(argv[1], IMREAD_COLOR);
+    Mat img_2 = imread(argv[2], IMREAD_COLOR);
+    assert(img_1.data && img_2.data && "Can not load images!");
+
+    vector<KeyPoint> keypoints_1, keypoints_2;
+    vector<DMatch> matches;
+    find_feature_matches(img_1, img_2, keypoints_1, keypoints_2, matches);
+    cout << "一共找到了" << matches.size() << "组匹配点" << endl;
+
+    // 建立3D点
+    Mat d1 = imread(argv[3], IMREAD_UNCHANGED);       // 深度图为16位无符号数，单通道图像
+    Mat K = (Mat_<double>(3, 3) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1);
+    vector<Point3f> pts_3d;
+    vector<Point2f> pts_2d;
+    for (DMatch m:matches) {
+        ushort d = d1.ptr<unsigned short>(int(keypoints_1[m.queryIdx].pt.y))[int(keypoints_1[m.queryIdx].pt.x)];
+        if (d == 0)   // bad depth
+            continue;
+        float dd = d / 5000.0;
+        Point2d p1 = pixel2cam(keypoints_1[m.queryIdx].pt, K);
+        pts_3d.push_back(Point3f(p1.x * dd, p1.y * dd, dd));
+        pts_2d.push_back(keypoints_2[m.trainIdx].pt);
+    }
+
+    // convert opencv point3d to eigen vector3d
+    VecVector3d pts_3d_eigen;
+    VecVector2d pts_2d_eigen;
+    for (size_t i = 0; i < pts_3d.size(); ++i) {
+        pts_3d_eigen.push_back(Eigen::Vector3d(pts_3d[i].x, pts_3d[i].y, pts_3d[i].z));
+        pts_2d_eigen.push_back(Eigen::Vector2d(pts_2d[i].x, pts_2d[i].y));
+    }
+
+    cout << "3d-2d pairs: " << pts_3d.size() << endl;
+    cout << "calling bundle adjustment by gauss newton" << endl;
+    Sophus::SE3d pose_gn;
+    chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+    t1 = chrono::steady_clock::now();
+    bundleAdjustmentGaussNewton(pts_3d_eigen, pts_2d_eigen, K, pose_gn);
+    chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+    cout << "solve pnp by gauss newton cost time: " << time_used.count() << " seconds." << endl;
+
+}
--- ch7/pose_estimation_3d2d.cpp
+++ ch7/pose_estimation_3d2d.cpp
@@ -3,10 +3,7 @@
 //
 
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/opencv.hpp>
 #include <Eigen/Core>
 #include <g2o/core/base_vertex.h>
 #include <g2o/core/base_unary_edge.h>
